7f1cea95a917f16e290ba16f530cdb953f67b7ae
ponfry <paschaotdel@gmail.com>
2016-05-10 15:30:35 +0300

    Исправлены последние ошибки

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-document.getElementById('button').onclick = function(){
-    var mail=document.getElementById('mail-id').value;
-    var password=document.getElementById("password-id").value;
-    alert("Mail:"+mail+"\nPassword:"+password);
-};
+$(document).ready(function() {
+    init();
+});
+
+function init() {
+    document.getElementsByClassName("login")[0].onclick = function() {
+        var mail = document.getElementById("mail-id").value;
+        var password = document.getElementById("password-id").value;
+
+        var message = "Mail:" + mail + "\nPassword:" + password;
+        alert(message);
+    };
+}
